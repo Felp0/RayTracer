16fca6ee3a18b4e8556b5d7ffe0955008ae2a09d
Fernandes <71331946+Felp0@users.noreply.github.com>
2022-12-04 22:40:29 +0000

    begginig of somehting

---
+++ RayTracerSmall/ThreadManager.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <thread>
+#include <vector>
+#include <functional>
+
+class ThreadManager
+{
+public:
+	//static void CreatingThread()
+
+private:
+	//static std::vector<std::thread*> m_threads;
+};
+
