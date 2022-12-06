9ad258b8ed48e38913e5ba04de0771d69a338fed
Fernandes <71331946+Felp0@users.noreply.github.com>
2022-12-06 20:18:10 +0000

    saving thers

--- RayTracerSmall/Queue.h
+++ RayTracerSmall/Queue.h
@@ -3,7 +3,7 @@
 
 
 template<typename T>
-
+//use shortcut to track work
 //Remember to delete the Node pointers
 class Queue
 {
@@ -31,10 +31,25 @@ public:
 		}
 
 		std::shared_ptr<T> const res(
-			std::make_shared<T>(std::move()));
-		std::unique_ptr<Node> const oldHead = std::move(head);
-		head = std::move( )
+			std::make_shared<T>(std::move(head->data)));
+		Node* const oldHead = std::move(head);
+		head = std::move(oldHead->pNext);
+		if (!head)
+		{
+			pTail = nullptr;
+		}
+
+		return res;
+	}
+
+	void push(T newValue)
+	{
+		//overload new functions so it malloc 
+		std::unique_ptr<Node> pNode(std::move(newValue)));
+		Node* const newTail = pNode.get();
+		if()
 	}
 
 private:
 };
+//https://learning.oreilly.com/library/view/c-concurrency-in/9781617294693/kindle_split_016.html#ch06lev1sec2
